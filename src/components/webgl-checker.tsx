430e37e882ceae20e7a5ec6e92883e2c003005a4
Tomáš Režňák <tomas.reznak@volny.cz>
2025-07-16 17:26:52 +0000

    Use modal instead of alert for WebGL check

--- src/components/webgl-checker.tsx
+++ src/components/webgl-checker.tsx
@@ -1,24 +1,71 @@
+import {
+  Button,
+  Checkbox,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  useDisclosure,
+} from "@heroui/react";
 import { useEffect } from "react";
 import { useLocalstorageState } from "rooks";
 import { isWebGL2Supported, isWebGLSupported } from "webgl-detector";
 
-export function WebGlChecker(): null {
+export function WebGlChecker(): React.ReactElement {
   const [skipWebGlCheck, setSkipWebGlCheck] = useLocalstorageState<boolean>(
     "skipWebGlCheck",
     false,
   );
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [checked, setChecked] = useLocalstorageState<boolean>(false);
 
   useEffect(() => {
     if (!skipWebGlCheck && !isWebGLSupported() && !isWebGL2Supported()) {
       setSkipWebGlCheck(true);
-      alert(
-        [
-          "Your browser does not support WebGL.",
-          "WebGL is recommended for the best experience on this site.",
-        ].join("\n\n"),
-      );
+      onOpen();
     }
   }, [skipWebGlCheck, setSkipWebGlCheck]);
 
-  return null;
+  return (
+    <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+      <ModalContent>
+        {(onClose) => (
+          <>
+            <ModalHeader className="flex flex-col gap-1">
+              WebGL not supported
+            </ModalHeader>
+            <ModalBody>
+              <p>
+                Your browser does not support WebGL, which is required for this
+                site to function properly.
+              </p>
+              <p>
+                Please use a modern browser with WebGL support, enable WebGL in
+                your browser settings, or update your current browser to the
+                latest version.
+              </p>
+            </ModalBody>
+            <ModalFooter className="flex flex-col gap-5">
+              <Checkbox
+                className="mt-4"
+                defaultSelected={false}
+                onChange={({ target: { checked } }) => setChecked(checked)}
+              >
+                Skip this check in the future
+              </Checkbox>
+              <Button
+                onPress={() => {
+                  onClose();
+                  setSkipWebGlCheck(checked);
+                }}
+              >
+                Dismiss
+              </Button>
+            </ModalFooter>
+          </>
+        )}
+      </ModalContent>
+    </Modal>
+  );
 }
