a4f736288ee3d203a2091bd5875bd259bbb921f7
Tomáš Režňák <tomas.reznak@volny.cz>
2026-03-19 20:11:53 +0100

    Replace devcontainer with nix flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            bun
+            pre-commit
+          ];
+
+          shellHook = ''
+            if ! [ -f .git/hooks/pre-commit ]; then
+              pre-commit install
+            fi
+          '';
+        };
+      }
+    );
+}
